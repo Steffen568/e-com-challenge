58d5472b35029632273e1482ec9d5b1e925115d0
Steffen568 <sgonzalez568@gmail.com>
2022-05-06 16:58:38 -0500

    product route added. not tested

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,25 +6,73 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    attributes: [
+      'id',
+      'product_name',
+      'price',
+      'stock',
+      'category_id'
+    ],
+    include: [
+      {
+        model: Category,
+        attributes: ['id', 'category_name']
+      },
+      {
+        model: Tag,
+        as: 'tagged_product',
+        attributes: ['id', 'tag_name']
+      }
+    ]
+  })
+  .then(dbProData => res.json(dbProData))
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err)
+  })
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'product_name',
+      'price',
+      'stock',
+      'category_id'
+    ],
+    include: [
+      {
+        model: Category,
+        attributes: ['id', 'category_name']
+      },
+      {
+        model: Tag,
+        as: 'tagged_product',
+        attributes: ['id', 'tag_name']
+      }
+    ]
+  })
+  .then(dbProData => {
+    if(!dbProData){
+      res.status(404).json({ message: 'No product found by this id' })
+    }
+    res.json(dbProData)
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // create new product
 router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
@@ -90,7 +138,22 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbProData => {
+    if (!dbProData) {
+      res.status(404).json({ message: 'No product found by this id' });
+      return;
+    }
+    res.json(dbProData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
